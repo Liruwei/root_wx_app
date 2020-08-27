665f946a8127c75b076d2fd74f8473b9e6cfd276
Ruwei Li <liruwei@liruweis-MacBook-Pro.local>
2020-08-27 18:54:45 +0800

    add: 支付结果页

--- pages/payment/success.js
+++ pages/payment/success.js
@@ -5,9 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    time: 10,
+    status: 0, // 0: 未知  1: 支付成功  2: 支付失败
+    progress: '0%'
   },
-
+  _timer: null,
   /**
    * 生命周期函数--监听页面加载
    */
@@ -19,7 +21,20 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    let that = this;
+    this.setData({
+      progress: '100%'
+    })
+    this._timer = setTimeout(() => {
+        that.setData({
+          status: 2,
+        });
+        wx.setNavigationBarTitle({
+          title: '支付失败',
+        });
+        clearTimeout(that._timer);
+        that._timer = null;
+    }, 1000 * 5);
   },
 
   /**
@@ -40,7 +55,10 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    if (this._timer) {
+      clearTimeout(this._timer);
+      this._timer = null;
+    }
   },
 
   /**
